39612341f637f2df3805b0b118bd8c1269bfa710
Vasyok <Algoritm>
2018-08-06 12:58:14 +0300

    feat(Creds): Updating configs

--- sockets.js
+++ sockets.js
@@ -109,15 +109,7 @@ module.exports = function (server, config) {
         var origin = client.handshake.headers.origin;
         if (!config.turnorigins || config.turnorigins.indexOf(origin) !== -1) {
             config.turnservers.forEach(function (server) {
-                var hmac = crypto.createHmac('sha1', server.secret);
-                // default to 86400 seconds timeout unless specified
-                var username = Math.floor(new Date().getTime() / 1000) + (parseInt(server.expiry || 86400, 10)) + "";
-                hmac.update(username);
-                credentials.push({
-                    username: username,
-                    credential: hmac.digest('base64'),
-                    urls: server.urls || server.url
-                });
+                credentials.push(server);
             });
         }
         client.emit('turnservers', credentials);
